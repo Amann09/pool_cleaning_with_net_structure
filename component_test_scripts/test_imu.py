84d9c649b9118bbf8f51dc7505af4734ba6f2df0
mohammad21saif <mohammad21@iiserb.ac.in>
2025-03-13 12:40:20 +0530

    added imu test

---
+++ component_test_scripts/test_imu.py
@@ -0,0 +1,16 @@
+from pymavlink import mavutil
+import math
+import numpy as np
+import time
+import scipy
+
+# Initialize MAVLink connection
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+master.wait_heartbeat()
+print("Heartbeat received from system (system %u component %u)" % (master.target_system, master.target_component))
+
+
+while True:
+    msg1 = master.recv_match(type='IMU_RAW', blocking=True)
+    print(msg1)
+    
